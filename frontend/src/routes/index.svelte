158973f20bf4e2ac116d56f95f2f34059c6bc0a6
Best-Verie <irabestverie2016@gmail.com>
2022-03-23 20:29:20 +0200

    setupppp

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const resp = await fetch(`http://backend:4000`);
+		const data = await resp.text();
+
+		return {
+			props: {
+				message: data
+			}
+		};
+	};
+</script>
+
+<script>
+	export let message;
+</script>
+
+<h1>Heyyyyyyyy</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
